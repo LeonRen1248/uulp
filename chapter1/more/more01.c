47b340504bf1ede2ed3aca39e8ae2a266e0bf846
Leon Ren <yuren1248@163.com>
2021-08-31 20:35:28 +0800

    修改完善了more的前2个版本

--- chapter1/more/more01.c
+++ chapter1/more/more01.c
@@ -4,57 +4,62 @@
 #define PAGELEN 24
 #define LINELEN 512
 
-void do_more(FILE *fp);
-int see_more();
+int see_more() {
+    printf("\033[7m --more?-- \033[m");
+    int c;
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') { // 再显示一行
+            return 1;
+        }
+        if (c == ' ') { // 再显示一整页
+            return PAGELEN;
+        }
+        if (c == 'q') { // 退出，不再显示
+            return 0;
+        }
+    }
 
-int main(int argc, char *argv[]) {
-    FILE *fp;
-    if (argc == 1) {
-        do_more(stdin);
-    }
-    while (--argc) {
-        if ((fp = fopen(*(++argv), "r")) != NULL) {
-            do_more(fp);
-            fclose(fp);
-        }
-        else {
-            exit(1);
-        }
-    }
-    
     return 0;
 }
 
 void do_more(FILE *fp) {
-    int num_line = 0, reply;
-    char line[LINELEN];
-    while (fgets(line, LINELEN, fp)) {
-        if (num_line >= PAGELEN) {
+    char buffer[LINELEN];
+    int cnt = 0;
+    int reply = 0;
+    while (fgets(buffer, LINELEN, fp) != NULL) {
+        if (cnt >= PAGELEN) {
             reply = see_more();
             if (reply == 0) {
                 break;
             }
-            else {
-                num_line -= reply;
-            }
+            cnt -= reply;
         }
-        if (fputs(line, stdout) == EOF) {
+        if ((fputs(buffer, stdout)) == EOF) {
             exit(1);
         }
-        num_line++;
+        cnt++;
+    }
+    if (reply) {
+        fputs("\n", stdout); // 增加一个换行，使终端提示信息显示在新一行中
     }
 }
 
-int see_more() {
-    printf("\033[7m more? \033[m");
-    char c;
-    while (c = getchar()) {
-        if (c == 'q')
-            return 0;
-        if (c == '\n')
-            return 1;
-        if (c == ' ')
-            return PAGELEN;
+int main(int argc, char const *argv[]) {
+    FILE *fp;
+    if (argc == 1) {
+        do_more(stdin);
     }
+    else {
+        while (--argc) {
+            if ((fp = fopen(*(++argv), "r")) != NULL) {
+                do_more(fp);
+                fclose(fp);
+            }
+            else {
+                exit(1);
+            }
+        }
+    }
+    
     return 0;
 }
--- chapter1/more/more02.c
+++ chapter1/more/more02.c
@@ -4,61 +4,69 @@
 #define PAGELEN 24
 #define LINELEN 512
 
-void do_more(FILE *fp);
-int see_more();
+// 为了解决 ls /bin | ./more.out 不会停止的问题，
+// 修改键盘的输入为从 /dev/tty 中读取，而不是从标准输入中读取
+int see_more(FILE *fp_tty) {
+    printf("\033[7m --more?-- \033[m");
+    int c;
+    while ((c = fgetc(fp_tty)) != EOF) {
+        if (c == '\n') { // 再显示一行
+            return 1;
+        }
+        if (c == ' ') { // 再显示一整页
+            return PAGELEN;
+        }
+        if (c == 'q') { // 退出，不再显示
+            return 0;
+        }
+    }
 
-int main(int argc, char *argv[]) {
-    FILE *fp;
-    if (argc == 1) {
-        do_more(stdin);
-    }
-    while (--argc) {
-        if ((fp = fopen(*(++argv), "r")) != NULL) {
-            do_more(fp);
-            fclose(fp);
-        }
-        else {
-            printf("\n");
-            exit(1);
-        }
-    }
-    
     return 0;
 }
 
 void do_more(FILE *fp) {
-    int num_line = 0, reply;
-    char line[LINELEN];
-    FILE *fd_tty = fopen("/dev/tty", "r");
-    while (fgets(line, LINELEN, fp)) {
-        if (num_line >= PAGELEN) {
-            reply = see_more(fd_tty);
+    char buffer[LINELEN];
+    int cnt = 0;
+    int reply = 0;
+    FILE *fp_tty = fopen("/dev/tty", "r");
+    while (fgets(buffer, LINELEN, fp) != NULL) {
+        if (cnt >= PAGELEN) {
+            reply = see_more(fp_tty);
             if (reply == 0) {
                 break;
             }
-            else {
-                num_line -= reply;
-            }
+            cnt -= reply;
         }
-        if (fputs(line, stdout) == EOF) {
-            exit(1);
+        if ((fputs(buffer, stdout)) == EOF) {
+            goto __ERROR__; // 异常处理
         }
-        num_line++;
+        cnt++;
     }
-    printf("\n");
-    fclose(fd_tty);
+    if (reply) {
+        fputs("\n", stdout); // 增加一个换行，使终端提示信息显示在新一行中
+    }
+__ERROR__:
+    if (fp_tty) {
+        fclose(fp_tty);
+    }
 }
 
-int see_more(FILE *cmd) {
-    printf("\033[7m more? \033[m");
-    char c;
-    while ((c = fgetc(cmd)) != EOF) {
-        if (c == 'q')
-            return 0;
-        if (c == '\n')
-            return 1;
-        if (c == ' ')
-            return PAGELEN;
+int main(int argc, char const *argv[]) {
+    FILE *fp;
+    if (argc == 1) {
+        do_more(stdin);
     }
+    else {
+        while (--argc) {
+            if ((fp = fopen(*(++argv), "r")) != NULL) {
+                do_more(fp);
+                fclose(fp);
+            }
+            else {
+                exit(1);
+            }
+        }
+    }
+    
     return 0;
 }
