cb63de8acfe26a93314098c6f4fde874b6064b15
LeonRen1248 <yuren1248@163.com>
2021-09-02 21:42:47 +0800

    fileinfo和第一个版本的ls（还不能列出长格式的文件信息）

--- chapter3/ls/fileinfo.c
+++ chapter3/ls/fileinfo.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <time.h>
-#include <string.h>
-#include <pwd.h>
 
-void show_stat_info(char *filename, struct stat *buf);
+void show_stat_info(const char *filename, struct stat fileinfo) {
+    printf("%10s: %o\n", "mode", fileinfo.st_mode);
+    printf("%10s: %ld\n", "links", fileinfo.st_nlink);
+    printf("%10s: %d\n", "user", fileinfo.st_uid);
+    printf("%10s: %d\n", "group", fileinfo.st_gid);
+    printf("%10s: %ld\n", "size", fileinfo.st_size);
+    char *time_str = ctime(&(fileinfo.st_mtim.tv_sec));
+    time_str[24] = '\0';
+    printf("%10s: %s\n", "modtime", time_str);
+    printf("%10s: %s\n", "name", filename);
+}
 
-int main(int argc, char **argv) {
-    struct stat info;
-    if (argc > 1) {
-        if (stat(argv[1], &info) != -1) {
-            show_stat_info(argv[1], &info);
+int main(int argc, char const *argv[]) {
+    if (argc == 1) {
+        fprintf(stderr, "Error: Tow few arguments.\n");
+        exit(1);
+    }
+    struct stat fileinfo;
+    while (--argc) {
+        int stat_ret = -1;
+        if ((stat_ret = stat(*(++argv), &fileinfo)) == -1) {
+            fprintf(stderr, "Error: Error get stat info of file: %s\n", *argv);
+            exit(1);
         }
-        else {
-            perror(argv[1]);
-        }
+        show_stat_info(*argv, fileinfo);
     }
     
     return 0;
-}
-
-void show_stat_info(char *filename, struct stat *buf) {
-    printf("%10s: %o\n", "mode", buf->st_mode);
-    printf("%10s: %ld\n", "links", buf->st_nlink);
-    printf("%10s: %d\n", "user", buf->st_uid);
-    printf("%10s: %d\n", "group", buf->st_gid);
-    printf("%10s: %ld\n", "size", buf->st_size);
-    char *curr_time = ctime(&buf->st_mtim.tv_sec);
-    curr_time[strlen(curr_time) - 1] = '\0';
-    printf("%10s: %s\n", "modtime", curr_time);
-    printf("%10s: %s\n", "name", filename);
 }
--- chapter3/ls/ls1.c
+++ chapter3/ls/ls1.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <dirent.h>
-#include <sys/types.h>
+#include <sys/stat.h>
 
-void do_ls(char *dirname);
+void do_ls(const char *path) {
+    DIR *dirp = NULL;
+    struct dirent *dirent_ptr;
+    if ((dirp = opendir(path)) == NULL) {
+        fprintf(stderr, "Error open directory %s.", path);
+        return;
+    }
+    while ((dirent_ptr = readdir(dirp)) != NULL) {
+        if (dirent_ptr->d_name[0] != '.') {
+            printf("%s\n", dirent_ptr->d_name);
+        }
+    }
+    closedir(dirp);
+}
 
-int main(int argc, char **argv) {
+int main(int argc, char const *argv[]) {
     if (argc == 1) {
         do_ls(".");
     }
-    while (--argc) {
-        printf("%s:\n", *(++argv));
-        do_ls(*argv);
+    else {
+        for (int i = 1; i < argc; i++) {
+            printf("==================== %s ====================\n", *(++argv));
+            do_ls(*argv);
+        }
     }
     
     return 0;
-}
-
-void do_ls(char *dirname) {
-    DIR *current_dir;
-    struct dirent *current_record;
-    if ((current_dir = opendir(dirname)) == NULL) {
-        fprintf(stderr, "ls1: cannot open %s\n", dirname);
-    }
-    while (current_record = readdir(current_dir)) {
-        printf("%s\n", current_record->d_name);
-    }
-    closedir(current_dir);
 }
